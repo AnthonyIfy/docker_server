3887086426c7a61fc5a548acf2f3ed24a1a214d7
AnthonyIfy <136186728+AnthonyIfy@users.noreply.github.com>
2023-07-01 23:46:36 -0500

    Update main.tf

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "Anthony-docker-tfstate"
+  bucket = "anthony-docker-tfstate"
      
   lifecycle {
     prevent_destroy = true
